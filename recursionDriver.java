e0efb812c041bd66ede1e61d0b6f55877b87ada6
pickledpeppers <31286555+pickledpeppers@users.noreply.github.com>
2018-10-31 11:24:25 -0700

    added recursionDriver

---
+++ recursionDriver.java
@@ -0,0 +1,30 @@
+public class recursionDriver
+{
+   
+    public static int count(int n)
+    {
+    }
+    
+    
+    
+    public static void main(String[] args)
+    {
+         /*
+          * Count 
+          * 1. Number of legs  10 elephant have
+          * 2. Number of students in a school if each
+          *    class has 20 students and there are 12 rooms
+          * 3. Number of fingers if there are 8 people
+          * 4. Find the sum of odd numbers from 0 to N
+          * 5. Find the sum of even numbers from 0 to N
+     
+          * 
+          */   
+         int counted=0;
+
+         // Task 1.
+         counted = count(10);
+         System.out.println(counted);
+
+    }
+}
