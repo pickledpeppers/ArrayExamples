e7b606968167c6f23266556b95ca8184f687d08d
pickledpeppers <31286555+pickledpeppers@users.noreply.github.com>
2018-11-05 11:57:01 -0800

    4

--- recursionDriver.java
+++ recursionDriver.java
@@ -1,11 +1,28 @@
 public class recursionDriver
 {
    
-    public static int count(int n)
+    public static int countElephant(int n)
     {
+        if (n == 0)
+        return 0;
+        return 2 + countElephant(n-1);
     }
     
+    public static int countOddNum(int n)
+    {
+        //given that n it is odd
+        if (n==1)
+        return 1;
+        return n + countOddNum(n-2);
+    }
     
+    public static int countEvenNum(int n)
+    {
+        //given that n it is even
+        if (n==0)
+        return 0;
+        return n + countEvenNum(n-2);
+    }
     
     public static void main(String[] args)
     {
@@ -23,8 +40,7 @@ public class recursionDriver
          int counted=0;
 
          // Task 1.
-         counted = count(10);
+         counted = countElephant(10);
          System.out.println(counted);
-
     }
 }
